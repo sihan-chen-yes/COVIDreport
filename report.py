57c92ce92d7d06c784ddff1da6131c541e1e5303
sihan <779136976@qq.com>
2022-03-15 01:03:30 +0800

    add email function

--- report.py
+++ report.py
@@ -1,14 +1,17 @@
 import requests
 import json
 from datetime import date
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
 
 login_url = "https://app.buaa.edu.cn/uc/wap/login?redirect=https%3A%2F%2Fapp.buaa.edu.cn%2Fsite%2FbuaaStudentNcov%2Findex"
 login_check_url = "https://app.buaa.edu.cn/uc/wap/login/check"
 info_url = "https://app.buaa.edu.cn/buaaxsncov/wap/default/get-info"
 save_url = "https://app.buaa.edu.cn/buaaxsncov/wap/default/save"
 
-username = "XX" #自己的账号
-password = "XX" #自己的密码
+#todo
+username = "XXX" #自己的账号
+password = "XXX" #自己的密码
 sess = requests.Session()
 path = './log.txt'
 
@@ -47,6 +50,34 @@ def post_info(info):
     f.write(str(res_info) + "\n")
     f.write("操作完毕" + "\n\n")
     f.close()
+    print(res_info['e'])
+    if res_info['e'] != 0:
+        sendMail()
+
+def sendMail():
+    #需要邮件服务器 可用QQ企业邮箱 todo
+    user = 'XXX'
+    password = 'XXX'
+    message = 'There\'s something wrong. Please do the COVID-19 report manually.'
+    Subject = 'reporter WA'
+    # 发送人
+    sender = 'reporter'
+    # 收件人
+    recipient = 'ito'
+    # 收件人邮箱地址 todo
+    to_addrs = 'XXX'
+    msg = MIMEText(message, 'plain', _charset="utf-8")
+    # 邮件主题描述
+    msg["Subject"] = Subject
+    # 发件人
+    msg["from"] = sender
+    # 收件人
+    msg["to"] = recipient
+    with SMTP_SSL(host="smtp.exmail.qq.com", port=465) as smtp:
+        # 登录发邮件服务器
+        smtp.login(user=user, password=password)
+        # 邮件配置
+        smtp.sendmail(from_addr=user, to_addrs=to_addrs.split(','), msg=msg.as_string())
 
 if __name__ == '__main__':
     login()
